bea22360afc85df74c4ef5c7f0402e5758c8370e
FirdausyTama <atama.firdausy@gmail.com>
2025-04-26 21:02:36 +0700

    membuat background dan tombol logout

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ucp1/auth/login_page.dart';
 
 class HomePage extends StatelessWidget {
   final String email;
@@ -8,25 +9,52 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              Row(
-                spacing: 15,
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              color: Colors.blue,
+              padding: const EdgeInsets.all(16),
+              child: Row(
                 children: [
                   CircleAvatar(
                     radius: 30,
                     backgroundImage: AssetImage('assets/images/profil.jpg'),
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [Text('Selamat Datang'), Text(email)],
+                  const SizedBox(width: 15),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Selamat Datang',
+                          style: TextStyle(color: Colors.white, fontSize: 16),
+                        ),
+                        Text(
+                          email,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  IconButton(
+                    icon: Icon(Icons.logout_outlined, color: Colors.white, size: 28),
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => LoginPage()),
+                      );
+                    },
                   ),
                 ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
