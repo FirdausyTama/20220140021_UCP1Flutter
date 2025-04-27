9cbe6c75930f585f5174e9c6072783b256e77429
FirdausyTama <atama.firdausy@gmail.com>
2025-04-27 15:32:30 +0700

    membuat textformfield data pelanggan

--- lib/pelanggan/data_pelanggan.dart
+++ lib/pelanggan/data_pelanggan.dart
@@ -20,6 +20,45 @@ class _DataPelangganState extends State<DataPelanggan> {
           style: TextStyle(color: Colors.white), 
         ),
       ),
+      body: Padding(padding: EdgeInsets.all(20),
+      child: Form(
+        child: 
+        Column(
+          children: [
+            Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 45),
+                  const Padding(
+                    padding: EdgeInsets.only(top: 16.0, bottom: 10.0),
+                    child: Text(
+                      'Nama Lengkap',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+              TextFormField(
+                //controller: namaController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  hintText: 'Nama Lengkap',
+                  prefixIcon: Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Icon(Icons.person),
+                  ),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Nama tidak boleh kosong';
+                  }
+                  return null;
+                },
+              ),
+          ],
+        )),),
     );
   }
 }
