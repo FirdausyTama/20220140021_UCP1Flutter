18279b063ab235c871e11c89cf5dd45300a4caba
FirdausyTama <atama.firdausy@gmail.com>
2025-04-27 19:42:21 +0700

    membuat datepicker dan dropdown formfield

--- lib/barang/data_barang.dart
+++ lib/barang/data_barang.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class DataBarang extends StatefulWidget {
   const DataBarang({super.key});
@@ -8,15 +9,165 @@ class DataBarang extends StatefulWidget {
 }
 
 class _DataBarangState extends State<DataBarang> {
+  final _formKey = GlobalKey<FormState>();
+  final ScrollController _scrollController = ScrollController();
+
+  DateTime? _selectedDate;
+  final List<Map<String, String>> piketData = [];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void addTask() {
+    setState(() {
+      piketData.add({
+        'tanggal': DateFormat(
+          'EEEE, d MMMM yyyy',
+          'id_ID',
+        ).format(_selectedDate!),
+      });
+      _selectedDate = null;
+    });
+
+    Future.delayed(const Duration(milliseconds: 300), () {
+      if (_scrollController.hasClients) {
+        _scrollController.animateTo(
+          _scrollController.position.maxScrollExtent,
+          duration: const Duration(milliseconds: 500),
+          curve: Curves.easeOut,
+        );
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(
-        backgroundColor: Colors.blue, 
-        centerTitle: true, 
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        centerTitle: true,
         iconTheme: IconThemeData(color: Colors.white),
-        title: Text(
-          'Pendataan Barang',
-          style: TextStyle(color: Colors.white), 
+        title: Text('Pendataan Barang', style: TextStyle(color: Colors.white)),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 20),
+              const Text(
+                'Tanggal Transaksi',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 8),
+              TextFormField(
+                readOnly: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  hintText:
+                      _selectedDate == null
+                          ? 'Tanggal Transaksi'
+                          : DateFormat(
+                            'EEEE, d MMMM yyyy',
+                            'id_ID',
+                          ).format(_selectedDate!),
+                  prefixIcon: const Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Icon(Icons.date_range_outlined),
+                  ),
+                ),
+                onTap: () async {
+                  final pickedDate = await showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime(2000),
+                    lastDate: DateTime(2100),
+                  );
+                  if (pickedDate != null) {
+                    setState(() {
+                      _selectedDate = pickedDate;
+                    });
+                  }
+                },
+                validator: (value) {
+                  if (_selectedDate == null) {
+                    return 'Tanggal tidak boleh kosong';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                'Jenis Transaksi',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 8),
+              DropdownButtonFormField<String>(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  hintText: 'Jenis Transaksi',
+                ),
+                items:
+                    ['Barang Masuk', 'Barang Keluar'].map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                onChanged: (value) {},
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Nama harus dipilih';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                'Jenis Barang',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 8),
+              DropdownButtonFormField<String>(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  hintText: 'Jenis Barang',
+                ),
+                items:
+                    ['Carrier', 'Sleeping Bag', 'Tenda', 'Sepatu'].map((
+                      String value,
+                    ) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                onChanged: (value) {},
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Jenis barang harus dipilih';
+                  }
+                  return null;
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
