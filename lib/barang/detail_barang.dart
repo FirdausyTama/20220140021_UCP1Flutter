ca0791cadd212ad848a0b6b59e6c8565d8d02b69
FirdausyTama <atama.firdausy@gmail.com>
2025-04-27 20:15:41 +0700

    membuat halaman detail barang stateless

---
+++ lib/barang/detail_barang.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DetailBarang extends StatelessWidget {
+  const DetailBarang({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
